e2e5dd14d7fffa60161994ab2323f0720edfeea7
Sebastián Gomez <sebastianga48@gmail.com>
2025-05-24 18:25:03 -0600

    modified pruebas

--- routes/api.php
+++ routes/api.php
@@ -4,15 +4,20 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ProfesorController;
 use App\Http\Controllers\Api\CursoController;
 use App\Http\Controllers\Api\AulaController;
-
+use App\Http\Controllers\Api\AuthController;
 
 Route::get('/test', function () {
     return response()->json(['message' => 'API funcionando']);
 });
-Route::apiResource('profesores', ProfesorController::class);
-Route::apiResource('cursos', CursoController::class);
-Route::apiResource('aulas', AulaController::class);
+
+// Rutas públicas
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
-Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
 
+// Rutas protegidas
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('profesores', ProfesorController::class);
+    Route::apiResource('cursos', CursoController::class);
+    Route::apiResource('aulas', AulaController::class);
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
